5004aeb4eb2e3b6255fb91aedac140af5fad8d38
Marc Ace J. Legaspi <90955780+mecsung@users.noreply.github.com>
2024-08-13 11:57:52 +0800

    oops

---
+++ down-server.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Under Maintenance</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <div class="box-image">
+        <div class="title">
+            <div class="main">Oops!</div>    
+            <div class="sub-title">Under Maintenance</div>
+        </div>
+        <div class="vector">
+            <img src="assets/building-website.png" alt="under construction">
+        </div>
+    </div>
+</body>
+</html>
